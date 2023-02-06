66224349445615588fe7abe72549446bbab4be87
atul <yenrumm@gmail.com>
2023-02-06 11:38:53 +0530

    script fix

--- pages/consent-orders.jsx
+++ pages/consent-orders.jsx
@@ -62,9 +62,10 @@ const ConsentOrdersEssentials = ({ blog }) => {
 
       <section className="w-full max-w-[800px] h-[220px] md:h-[450px] mx-auto block bg-gray-100 lg:my-3 relative">
         <iframe
-          src="https://player.vimeo.com/video/795834795?h=58ed90dafa&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479” frameborder=“0"
+          src="https://player.vimeo.com/video/795834795?h=58ed90dafa&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          frameborder="0"
           allow="autoplay; fullscreen; picture-in-picture"
-          allowfullscreen
+          allowFullscreen
           width="100%"
           height="100%"
           title="Kerryann On Getting Consent Orders"
